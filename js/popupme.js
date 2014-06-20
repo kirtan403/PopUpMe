4da340a79bf8526a1c1c783913fe03e2ec051723
Kirtan <kirtanthakkar4u@gmail.com>
2014-06-21 01:16:35 +0530

    Added Animation support
    Optimized multiple calls for the exiting the popup and added animations
    with initial state to final state and remove state.

--- js/popupme.js
+++ js/popupme.js
@@ -1,4 +1,4 @@
-// PopUpMe v1.0.0  |  (c) 2014 Kirtan Thakkar | MIT Licence
+// PopUpMe v1.1.0  |  (c) 2014 Kirtan Thakkar | MIT Licence
 $(document).ready(function(){
 	(function ( $ ) {
 			
@@ -10,7 +10,7 @@ $(document).ready(function(){
 				if(bgcount==0)
 				{
 					var htmlForBG = '<div id="popupme-bg" class="popupme-background popupme-hide"></div>';
-					var htmlForPopup = '<div class="popupme popupme-hide"><div style="position:relative;"><div class="popupme-close" >X</div><div class="popupme-title"></div><div class="popupme-text"></div></div></div>';
+					var htmlForPopup = '<div class="popupme popupme-opacity-0"><div style="position:relative;"><div class="popupme-close" >X</div><div class="popupme-title"></div><div class="popupme-text"></div></div></div>';
 					
 					$('body').append(htmlForBG);
 					$('body').append(htmlForPopup);
@@ -21,6 +21,9 @@ $(document).ready(function(){
 				$(window).resize(function(){
 					popupme_center();
 				});
+				
+				var topPer,leftPer;
+				var anim = param.animation;
 				
 				function popupme_center()
 				{
@@ -40,7 +43,7 @@ $(document).ready(function(){
 					var topPer = (((winHeight - popupHeight) / winHeight) * 100 ) / 2 ;
 					
 					$(".popupme").css('left',leftPer+"%");
-					$(".popupme").css('top',topPer+"%");	
+					$(".popupme").css('top',topPer+"%");
 				}
 				
 				$(this).on('click',function(event){
@@ -51,16 +54,58 @@ $(document).ready(function(){
 					
 					popupme_center();  //Centering after setting text
 					
-					$("#popupme-bg").removeClass("popupme-hide").addClass("popupme-show-5",0);
-					$(".popupme").removeClass("popupme-hide").addClass("popupme-show",0);
+					$("#popupme-bg").removeClass("popupme-hide").addClass("popupme-show-5");
+					
+					//removing previosly added classes for the animations
+					$(".popupme").attr('class', 'popupme popupme-opacity-0');
+					
+					if(typeof(anim) == "undefined" || anim == null || anim == "fade-in")
+					{
+						$(".popupme").show().addClass("popupme-fadein");
+					}
+					else if(anim == "zoom-in")
+					{
+						$(".popupme").addClass("popupme-zoomin-set").show().addClass("popupme-zoomin");
+					}
+					else if(anim == "zoom-out")
+					{
+						$(".popupme").addClass("popupme-zoomout-set").show().addClass("popupme-zoomout");
+					}
+					else if(anim == "rotate")
+					{
+						$(".popupme").addClass("popupme-rotate-set").show().addClass("popupme-rotate");
+					}
+					
+					
+				
+					//----------Exiting Click function on background and close button-------------
+						$("#popupme-bg,.popupme .popupme-close").on('click',function(){
+							
+							$("#popupme-bg").removeClass("popupme-show-5").addClass("popupme-hide");
+							
+							if(typeof(anim) == "undefined" || anim == null || anim == "fade-in")
+							{
+								$(".popupme").removeClass("popupme-fadein").addClass("popupme-fadein-remove");
+							}
+							else if( anim == 'zoom-in' )
+							{
+								$(".popupme").removeClass("popupme-zoomin").addClass("popupme-zoomin-remove");
+							}
+							else if( anim == 'zoom-out' )
+							{
+								$(".popupme").removeClass("popupme-zoomout").addClass("popupme-zoomout-remove");
+							}
+							else if( anim == 'rotate' )
+							{
+								$(".popupme").removeClass("popupme-rotate").addClass("popupme-rotate-remove");
+							}
+							
+							//On Exit remove click binding to remove redundant call for all the popups
+							$("#popupme-bg,.popupme .popupme-close").off('click');
+							
+						});
+					
 				});
-					
-				$("#popupme-bg,.popupme .popupme-close").on('click',function(){
-					
-					$("#popupme-bg").removeClass("popupme-show-5").addClass("popupme-hide",0);
-					$(".popupme").removeClass("popupme-show").addClass("popupme-hide",0);
-				});
-					
 				
 			
 			};
